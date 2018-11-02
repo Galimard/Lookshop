5e8747360a3a137c9a65249fb68553e81129359c
Maria <galimard.maria@gmail.com>
2018-11-02 15:26:14 +0800

    скрипт по вкладкам на карусели готов

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    //карусель в tabs-product
+
+    $('.carousel').slick({
+        setting-name: setting-value,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
+});
