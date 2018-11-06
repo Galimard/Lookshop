371db81e362e6c288ae53e4c5b8d5690d79f8b3e
Maria <galimard.maria@gmail.com>
2018-11-06 11:14:03 +0800

    пробный вариант слайдера в хедере

--- js/carousel.js
+++ js/carousel.js
@@ -2,13 +2,48 @@ $(document).ready(function(){
 
    //карусель в tabs-product
 
-    $('.carousel').slick({
-        appendArrows: $('.controls'),
-        prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
-        nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1
+    // $('.carousel').slick({
+    //     arrows: true,
+    //     appendArrows: $('.controls'),
+    //     prevArrow: '<a href="#" class="controls__link controls-link__left">налево</a>',
+    //     nextArrow: '<a href="#" class="controls__link controls-link__right">направо</a>',
+    //     infinite: true,
+    //     slidesToShow: 6,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //         {
+    //             breakpoint: 1024,
+    //             settings: {
+    //                 slidesToShow: 3,
+    //                 slidesToScroll: 1,
+    //                 infinite: false,
+    //                 arrows: true,
+    //                 dots: false
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 700,
+    //             settings: {
+    //                 arrows: true,
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //             }
+    //         }
+    // });
+
+    $('.header-carousel').slick({
+        arrows: true,
+        dots: true,
+        // dotsClass: 'header-carousel__indicator'
+        prevArrow: '<a href="#" class="header-carousel__prev"></a>',
+        nextArrow: '<a href="#" class="header-carousel__next"></a>',
     });
 
 });
