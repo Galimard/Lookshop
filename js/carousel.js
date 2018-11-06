c49735e6a7969709c94c75cd09d43505a25b5853
Maria <galimard.maria@gmail.com>
2018-11-06 14:39:36 +0800

    сделан слайдер в первой вкладке carousel-product

--- js/carousel.js
+++ js/carousel.js
@@ -1,49 +1,50 @@
 $(document).ready(function(){
 
+    //слайдер в хедере
+
+    $('.header-carousel').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '<a href="#" class="header-carousel__prev"></a>',
+        nextArrow: '<a href="#" class="header-carousel__next"></a>',
+    });
+
    //карусель в tabs-product
 
     // $('.carousel').slick({
     //     arrows: true,
     //     appendArrows: $('.controls'),
-    //     prevArrow: '<a href="#" class="controls__link controls-link__left">налево</a>',
-    //     nextArrow: '<a href="#" class="controls__link controls-link__right">направо</a>',
+    //     prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
+    //     nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
     //     infinite: true,
-    //     slidesToShow: 6,
+    //     slidesToShow: 5,
     //     slidesToScroll: 1,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1024,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //                 slidesToScroll: 1,
-    //                 infinite: false,
-    //                 arrows: true,
-    //                 dots: false
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 700,
-    //             settings: {
-    //                 arrows: true,
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 480,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //             }
-    //         }
+    //     // responsive: [
+    //     //     {
+    //     //         breakpoint: 1024,
+    //     //         settings: {
+    //     //             slidesToShow: 3,
+    //     //             slidesToScroll: 1,
+    //     //             infinite: false,
+    //     //             arrows: true,
+    //     //             dots: false
+    //     //         }
+    //     //     },
+    //     //     {
+    //     //         breakpoint: 700,
+    //     //         settings: {
+    //     //             arrows: true,
+    //     //             slidesToShow: 2,
+    //     //             slidesToScroll: 2
+    //     //         }
+    //     //     },
+    //     //     {
+    //     //         breakpoint: 480,
+    //     //         settings: {
+    //     //             slidesToShow: 1,
+    //     //             slidesToScroll: 1
+    //     //         }
+    //     //     }]
     // });
-
-    $('.header-carousel').slick({
-        arrows: true,
-        dots: true,
-        // dotsClass: 'header-carousel__indicator'
-        prevArrow: '<a href="#" class="header-carousel__prev"></a>',
-        nextArrow: '<a href="#" class="header-carousel__next"></a>',
-    });
 
 });
--- js/script.js
+++ js/script.js
@@ -3,11 +3,28 @@ $(document).ready(function(){
     //переключение вкладок в tabs-product
 
     $('.tabs__menu-link').click(function(e) { //при клике на ссылку таба
+       var tab = $(this).attr('href'); //достаем адрес нужного блока с новым текстом
         e.preventDefault(); //останавливаем загрузку страницы сслыки. Правильно?
         $('.tabs__menu-link--active').removeClass('tabs__menu-link--active'); //находим все ссылки с данным классом и удаляем класс актив
         $(this).addClass('tabs__menu-link--active'); //у нажатой ссылки добавляем класс актив
-        const tab = $(this).attr('href'); //достаем адрес нужного блока с новым текстом
-        $(tab).css({'display':'flex'}); //показываем текущую вкладку
-        $('.carousel').not(tab).css({'display':'none'}); //скрываем остальные вкладки, если честно, не понимаю почему эту строчку пришлось добавить, но без нее правильно не работало
+
+
+       $(tab).css({'display':'flex'}); //показываем текущую вкладку
+       $('.carousel').not(tab).css({'display':'none'}); //скрываем остальные вкладки, если честно, не понимаю почему эту строчку пришлось добавить, но без нее правильно не работало
+    });
+
+    $('.header-carousel').slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '<a href="#" class="header-carousel__prev"></a>',
+        nextArrow: '<a href="#" class="header-carousel__next"></a>',
+    });
+
+    $('#carousel-one').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        appendArrows: $('.controls'),
+        prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
+        nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
     });
 });
