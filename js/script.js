e881c06aa639dd901d4b18bb59deb44c072006ad
Maria <galimard.maria@gmail.com>
2018-11-07 14:06:04 +0800

    #fix допилен скрипт слайдеров в табах, поправлены кнопки Купить, снова

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
 $(document).ready(function() {
 
+    initializeTabs();
+
     //переключение вкладок в tabs-product
+    $('.tabs__menu-link').click(function(e) {
+        e.preventDefault();
 
-    $('.tabs__menu-link').click(function(e) { //при клике на ссылку таба
-        e.preventDefault(); //останавливаем загрузку страницы сслыки
+        var $item = $(this).closest('.tabs-menu__item'),
+            $contentItems = $('.carousel'),
+            itemPosition = $($item).data('class'),
+            $currentTab = false;
 
-        var item = $(this).closest('.tabs-menu__item');
-        var contentItem = $('.carousel');
-        var itemPos = item.data('class');
-
-        contentItem.filter('.carousel-'+itemPos)
-            .add(item)
-            .addClass('carousel--active')
-            .siblings()
-            .removeClass('carousel--active');
-
-        $('.carousel-first').slick({
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            appendArrows: $('.controls'),
-            prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
-            nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
-        });
+        $($item).siblings().removeClass('tabs-menu__item--active');
+        $($item).addClass('tabs-menu__item--active');
+        $currentTab = $contentItems.filter('.carousel-' + itemPosition);
+        $currentTab.siblings().removeClass('carousel--active');
+        $currentTab.addClass('carousel--active');
+        initializeCurrentSlider($currentTab);
     });
 
     //слайдер в хедере
-
     $('.header-carousel').slick({
         arrows: true,
         dots: true,
@@ -35,69 +29,29 @@ $(document).ready(function() {
 
 });
 
-    //слайдер в carousel-product
+//Первоначальная инициализация слайдеров табов
+function initializeTabs(){
+    $('.carousel--active').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        appendArrows: $('.controls'),
+        prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
+        nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
+    });
+}
 
+//Инициализирует слайдер на активной вкладке
+//@param $currentTab (jquery selector) - текущий селектор для карусели на вкладке
+function initializeCurrentSlider($currentTab){
+    'use strict';
+    $('.carousel.slick-slider').slick('destroy');
 
+    $($currentTab).slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        appendArrows: $('.controls'),
+        prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
+        nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
+    });
 
-//     $('.carousel-second').slick({
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         appendArrows: $('.controls'),
-//         prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
-//         nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
-//     });
-//
-//     $('.carousel-third').slick({
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         appendArrows: $('.controls'),
-//         prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
-//         nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
-//     });
-//
-//     $('.carousel-fourth').slick({
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         appendArrows: $('.controls'),
-//         prevArrow: '<a href="#" class="controls__link controls-link__left"></a>',
-//         nextArrow: '<a href="#" class="controls__link controls-link__right"></a>',
-//     });
-// });
-
-//responsive: [
-//     //     {
-//     //         breakpoint: 1024,
-//     //         settings: {
-//     //             slidesToShow: 3,
-//     //             slidesToScroll: 1,
-//     //             infinite: false,
-//     //             arrows: true,
-//     //             dots: false
-//     //         }
-//     //     },
-//     //     {
-//     //         breakpoint: 700,
-//     //         settings: {
-//     //             arrows: true,
-//     //             slidesToShow: 2,
-//     //             slidesToScroll: 2
-//     //         }
-//     //     },
-//     //     {
-//     //         breakpoint: 480,
-//     //         settings: {
-//     //             slidesToShow: 1,
-//     //             slidesToScroll: 1
-//     //         }
-//     //     }]
-
-// $('.tabs__menu-link').click(function(e) { //при клике на ссылку таба
-//     e.preventDefault(); //останавливаем загрузку страницы сслыки
-//
-//     var tab = $(this).attr('href'); //достаем адрес нужного блока с новым текстом
-//
-//     $('.tabs__menu-link--active').removeClass('tabs__menu-link--active'); //находим все ссылки с данным классом и удаляем класс актив
-//     $(this).addClass('tabs__menu-link--active'); //у нажатой ссылки добавляем класс актив
-//     $(tab).css({'display':'flex'}); //показываем текущую вкладку
-//     $('.carousel').not(tab).css({'display':'none'}); //скрываем остальные вкладки, если честно, не понимаю почему эту строчку пришлось добавить, но без нее правильно не работало
-// });
+}
